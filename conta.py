c18b93d7900b0d3d05456744ff193d81deaf5ba3
Wagner Filho <wagnerbhf@gmail.com>
2023-12-03 15:39:48 -0300

    Add files via upload

---
+++ conta.py
@@ -0,0 +1,56 @@
+class Conta:
+    def __init__(self, numero, titular, saldo, limite):
+        print("Construindo objeto ... {}".format(self))
+        self.__numero = numero
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
+        self.__codigo_banco = "001"
+
+    def extrato(self):
+        print("Saldo {} do titular {}".format(self.__saldo, self.__titular))
+
+    def depositar(self, valor):
+        self.__saldo += valor
+
+    def __pode_sacar(self, valor_a_sacar):
+        valor_disponivel_a_sacar = self.__saldo + self.__limite
+        return valor_a_sacar <= valor_disponivel_a_sacar
+
+    def saca(self, valor):
+        if (self.__pode_sacar(valor)):
+            self.__saldo -= valor
+        else:
+            print("O valor {} passou o limite".format(valor))
+
+    def transfere(self, valor, destino):
+        self.saca(valor)
+        destino.deposita(valor)
+
+    @property
+    def titular(self):
+        return self.titular
+
+    @property
+    def saldo(self):
+        return self.saldo
+
+    @property
+    def limite(self):
+        return self.limite
+
+    @limite.setter
+    def limite(self, limite):
+        self.__limite = limite
+
+    @property
+    def codigo_banco(self):
+        return self.__codigo_banco
+
+    @staticmethod
+    def codigo_banco():
+        return "001"
+
+    @staticmethod
+    def codigos_bancos():
+        return {'BB': '001', 'Caixa': '104', 'Bradesco': '237'}
---
+++ main.py
@@ -0,0 +1,6 @@
+from conta import Conta
+
+conta1 = Conta(1, "Wagner", 55.5, 1000.0)
+
+print(conta1.codigo_banco())
+print(conta1.codigos_bancos())
